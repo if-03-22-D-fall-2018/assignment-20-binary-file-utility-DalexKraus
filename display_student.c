43d50f01bad07bb0422fb06760ec0fa091ee2702
DalexKraus <dalex.kraus@gmail.com>
2019-06-05 22:30:46 +0200

    Added Display_Student

--- display_student.c
+++ display_student.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "generic.h"
 
 int main(int argc, const char* argv[])
@@ -9,18 +10,18 @@ int main(int argc, const char* argv[])
 
     if (argc == 1)
         return -1;
-    int id = atoi(argv[1]);
+    int id = atoi(argv[1]) - 1;
 
-    size_t student_size = sizeof(firstName) + sizeof(lastName) + sizeof(int);
+    size_t student_size = sizeof(firstName) + sizeof(lastName) + sizeof(int) * 2;
 
     FILE* fd = fopen("student.dat", "r");
-    fseek(fd, sizeof(int) + student_size * id);
+    fseek(fd, sizeof(int) + student_size * id, SEEK_SET);
 
     fread(firstName, sizeof(char), sizeof(firstName), fd);
     fread(lastName, sizeof(char), sizeof(lastName), fd);
     fread(&age, sizeof(int), 1, fd);
 
-    printf("First Name: %s\nLast Name: %s\nAge: %d", firstName, lastName, age);
+    printf("First Name: %s\nLast Name: %s\nAge: %d\n", firstName, lastName, age);
 
     fclose(fd);
     return 0;
